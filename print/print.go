15653eeaee42b3bc7cc8dd78760c13c7e89f4642
tyoko <2511649+taka-yoko@users.noreply.github.com>
2024-03-05 02:00:32 +0900

    printまで

---
+++ print/print.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 123
+	str := "ABC"
+
+	fmt.Print("num=", num, " str=", str, "\n")
+	fmt.Println("num =", num, "str =", str)
+	fmt.Printf("num=%d str=%s\n", num, str)
+}
