870ddb2354d1ea8ec4ec8c696f894fb125386425
tyoko <2511649+taka-yoko@users.noreply.github.com>
2024-03-07 21:37:48 +0900

    interfaceによるポリモーフィズム

--- interface/interface.go
+++ interface/interface.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
+type Printable interface {
+	ToString() string
+}
+
+func PrintOut(p Printable) {
+	fmt.Println(p.ToString())
+}
+
 type Person struct {
 	name string
 }
 
 func (p Person) ToString() string {
 	return p.name
-}
-func (p Person) PrintOut() {
-	fmt.Println(p.ToString())
 }
 
 type Book struct {
@@ -20,13 +25,10 @@ type Book struct {
 func (b Book) ToString() string {
 	return b.title
 }
-func (b Book) PrintOut() {
-	fmt.Println(b.ToString())
-}
 
 func main() {
 	a1 := Person{name: "司馬遼太郎"}
 	a2 := Book{title: "新記太閤記"}
-	a1.PrintOut()
-	a2.PrintOut()
+	PrintOut(a1)
+	PrintOut(a2)
 }
